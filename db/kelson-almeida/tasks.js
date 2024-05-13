eda3e98c91a991594c2459edcfd190e329ceec5b
Kelson Almeida <54041512+kelsonvictr@users.noreply.github.com>
2024-05-13 11:25:22 -0300

    Update tasks.js

--- db/kelson-almeida/tasks.js
+++ db/kelson-almeida/tasks.js
@@ -1,19 +1,7 @@
 var kelson_almeida_tasks = [
     {
-        "name": "Testando 1",
-        "description": "Descrição desse card",
-        "responsible": "Prof. Kelson",
-        "status": "backlog"
-    },
-    {
-        "name": "Testando 2",
-        "description": "Descrição desse card",
-        "responsible": "Prof. Kelson",
-        "status": "in_progress"
-    },
-    {
-        "name": "Testando 3",
-        "description": "Descrição desse card",
+        "name": "Abrir inscrições da Turma 2",
+        "description": "Turma 2 com inscrições abertas!",
         "responsible": "Prof. Kelson",
         "status": "concluded"
     },
