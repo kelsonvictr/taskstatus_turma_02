c30805b72ab410d34d0103fb1aa0cc5fb8c30fc7
Daniela <danielabna@gmail.com>
2024-05-13 16:42:36 -0300

    Adicionando card do desafio do site de um restaurante/lanchonete

--- db/daniela-barreto/tasks.js
+++ db/daniela-barreto/tasks.js
@@ -10,5 +10,11 @@ var daniela_barreto_tasks = [
         "description": "Mudar cores e outros estilos do CV da aula 01; Gravar um vídeo da tela e voz explicando as alterações e código; Criar repositório publico no github: curso-fullstack-desafios; Enviar o link no classroom",
         "responsible": "Daniela Barrêto",
         "status": "backlog"
+    },
+    {
+        "name": "Aula 01: Fazer desafio do site de um restaurante/lanchonete",
+        "description": "Criar um site para um restaurante ou lanchonete (real ou ficticio)",
+        "responsible": "Daniela Barrêto",
+        "status": "backlog"
     }
 ];
