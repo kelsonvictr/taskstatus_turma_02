1cf216137cf92116e8ba0ee0833b3c3dd1081677
tbandrade85 <tbandrade85@gmail.com>
2024-05-19 10:36:16 -0300

    Card Modificado

---
+++ db/thiago-andrade/tasks.js
@@ -0,0 +1,27 @@
+var thiago_andrade_tasks = [
+    {
+        "name": "Algorimitos elógicas de programação",
+        "description": "Algoritimos e lógica de programação - Nelio Alves",
+        "responsible": "Thiago Andrade",
+        "status": "in_progress"
+    },
+    {
+        "name": "Desafio Currículo",
+        "description": "Desafio Currículo da Aula 01",
+        "responsible": "Thiago Andrade",
+        "status": "conclued"
+    },
+    {
+        "name": "Desafio site de  Restaurante",
+        "description": "Desafio site de  restaurante - Aula 01",
+        "responsible": "Thiago Andrade",
+        "status": "in_progress"
+    },
+    {
+        "name": "Estudar HTML e CSS",
+        "description": "Estudar HTML e CSS",
+        "responsible": "Thiago Andrade",
+        "status": "in_progress"
+    },
+    
+];
--- scripts/loadTasks.js
+++ scripts/loadTasks.js
@@ -9,7 +9,8 @@ const teamMembers = [
     'luisa-silva',
     'tatiane-fontes',
     'brasimar-henrique',
-    'luiz-reis'
+    'luiz-reis',
+    'thiago-andrade'
 ];
 
 teamMembers.forEach(member => {
