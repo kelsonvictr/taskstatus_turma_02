329df218ba7a6eab1a92bba3aa0e795644fba091
Giordano <giordanopalmezano@gmail.com>
2024-05-19 15:43:29 -0300

    Adicionando novo card de revisão da aula01

---
+++ db/giordano-ferreira/tasks.js
@@ -0,0 +1,8 @@
+var giordano_ferreira_tasks = [
+    {
+        "name": "Revisão aula1 e desafios",
+        "description": "Estudando conteúdo aula1 e desenvolvendo desafios",
+        "responsible": "Giordano Ferreira",
+        "status": "in_progress"
+    },
+];
