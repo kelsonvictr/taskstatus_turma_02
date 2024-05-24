6749b0a614ab16ea5d96f3e7800d6054a5872b24
carolineandradecosta <andradecostacaroline@gmail.com>
2024-05-24 17:13:20 -0300

    criando o card do desafio do curriculo

--- db/caroline-andrade/tasks.js
+++ db/caroline-andrade/tasks.js
@@ -4,5 +4,12 @@ var caroline_andrade_tasks = [
         "description": "Spring Boot 3: aplique boas práticas e proteja uma API Rest",
         "responsible": "Caroline Andrade",
         "status": "backlog"
+    },
+
+    {
+        "name": "Desafio Currículo",
+        "description": "Fazer um currículo utilizando HTML5 e CSS3",
+        "responsible": "Caroline Andrade",
+        "status": "concluded"
     }
 ];
