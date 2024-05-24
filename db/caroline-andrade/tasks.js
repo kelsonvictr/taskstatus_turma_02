16c859e77f170f4d2c76e0a4025d2ca3a2c38199
carolineandradecosta <andradecostacaroline@gmail.com>
2024-05-24 17:24:21 -0300

    adicionando o card do desafio do site do restaurante

--- db/caroline-andrade/tasks.js
+++ db/caroline-andrade/tasks.js
@@ -11,5 +11,12 @@ var caroline_andrade_tasks = [
         "description": "Fazer um currículo utilizando HTML5 e CSS3",
         "responsible": "Caroline Andrade",
         "status": "concluded"
+    },
+
+    {
+        "name": "Desafio Site Restaurante",
+        "description": "Fazer um site fictício de um restaurante ou lanchonete",
+        "responsible": "Caroline Andrade",
+        "status": "concluded"
     }
 ];
