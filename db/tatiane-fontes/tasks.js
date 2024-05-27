42e32b94bace2bb0afecd3b376713e77357fb33f
Tatiane <thathy23@gmail.com>
2024-05-27 15:46:37 -0300

    Atualiazando cards

--- db/tatiane-fontes/tasks.js
+++ db/tatiane-fontes/tasks.js
@@ -1,9 +1,9 @@
 var tatiane_fontes_tasks = [
     {
-        "name": "Praticar Git/Github ",
+        "name": "Praticando Git/Github ",
         "description": "Usar o Github praticando os comandos mais utilizados",
         "responsible": "Tatiane Fontes",
-        "status": "backlog"
+        "status": "in_progress"
     },
     {
         "name": "Desafio Currículo",
@@ -12,9 +12,21 @@ var tatiane_fontes_tasks = [
         "status": "in_progress"
     },
     {
-        "name": "Desafio Landing Page Restaurante",
-        "description": "Desenvolver um site de restaurante apenas com CSS e HTML",
+        "name": "Fazer deploy do site restaurante",
+        "description": "Deploy com a ferramenta vercel",
         "responsible": "Tatiane Fontes",
         "status": "in_progress"
-    }
+    },
+    {
+        "name": "Desafio Landing Page Restaurante",
+        "description": "Site de restaurante criado com CSS e HTML",
+        "responsible": "Tatiane Fontes",
+        "status": "concluded"
+    },
+    {
+        "name": "Gravar com OBS explicação da Landing Page Restaurante",
+        "description": "Gravar com OBS explicação do site",
+        "responsible": "Tatiane Fontes",
+        "status": "backlog"
+    },
 ];
