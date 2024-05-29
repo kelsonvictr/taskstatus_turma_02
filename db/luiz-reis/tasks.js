429630aa007631ff3d8bc00490558e4c2702cf99
LuizHReis03 <luizhenriquereis70@gmail.com>
2024-05-29 16:09:09 -0300

    Desafios Concluídos

--- db/luiz-reis/tasks.js
+++ db/luiz-reis/tasks.js
@@ -3,13 +3,13 @@ var luiz_reis_tasks = [
         "name": "Desafio do site de restaurante",
         "description": "Criar um site de um restaurante ou lanchonete",
         "responsible": "Luiz Reis",
-        "status": "in_progress"
+        "status": "Concluded"
     },
 
     {
         "name": "Desafio do Currículo",
         "description": "Criar um Currículo com html, java e javascript",
         "responsible": "Luiz Reis",
-        "status": "in_progress"
+        "status": "Concluded"
     }
 ];
