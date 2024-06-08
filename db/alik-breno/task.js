38bbdddbfb010de37975e0b5455107f33ded8db5
Breno Chacon <brenochacon4@gmail.com>
2024-06-08 11:11:23 -0300

    new card in progress

--- db/alik-breno/task.js
+++
@@ -1,8 +0,0 @@
-var alik_breno_tasks = [
-    {
-        "name": "Desafio Restaurante",
-        "description": "Desenvolver um site de restaurante",
-        "responsible": "Alik Breno",
-        "status": "backlog"
-    }
-];
