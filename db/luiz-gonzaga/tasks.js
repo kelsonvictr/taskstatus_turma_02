1b4b10121a2833938b8a316ba4fcf66d60dec86d
Luizribeiro05 <112145713+Luizribeiro05@users.noreply.github.com>
2024-06-08 22:51:21 -0300

    Iniciando os exercicios JS

--- db/luiz-gonzaga/tasks.js
+++ db/luiz-gonzaga/tasks.js
@@ -15,6 +15,13 @@ var luiz_gonzaga_tasks = [
         "name": "Desafio - Hospedar Site",
         "description": "Desafio - Hospedar Site do Restaurante na Vercel",
         "responsible": "Luiz Gonzaga",
+        "status": "concluded"
+    },
+    {
+        "name": "150 Exercicios JS / DOM",
+        "description": "Resolução dos exercicos de Javascript / DOM .",
+        "repository" : "https://github.com/Luizribeiro05/100_exercicios_js",
+        "responsible": "Luiz Gonzaga",
         "status": "in_progress"
     }
 ];
