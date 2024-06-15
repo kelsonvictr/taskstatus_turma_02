ed9834c4925bf6ec1025d1d12a841eddb06dee4e
ArturLucas09 <artur_santos159@hotmail.com>
2024-06-15 01:04:54 -0300

    adicionando card dos exercicios de js

--- db/artur-lucas/tasks.js
+++ db/artur-lucas/tasks.js
@@ -1,21 +1,28 @@
 var artur_lucas_tasks = [
   {
-    name: "Curso lógica e algoritmos",
-    description: "Curso udemy logica de programação",
-    responsible: "Artur Lucas",
-    status: "in_progress",
+    "name": "Curso lógica e algoritmos",
+    "description": "Curso udemy logica de programação",
+    "responsible": "Artur Lucas",
+    "status": "in_progress",
   },
 
   {
-    name: "Exercícios Html e Css",
-    description: "Praticando exercicios html e css",
-    responsible: "Artur Lucas",
-    status: "in_progress",
+    "name": "Exercícios Html e Css",
+    "description": "Praticando exercicios html e css",
+    "responsible": "Artur Lucas",
+    "status": "concluded",
   },
   {
-    name: "Curso git/github",
-    description: "Aprender git/github",
-    responsible: "Artur Lucas",
-    status: "backlog",
+    "name": "Curso git/github",
+    "description": "Aprender git/github",
+    "responsible": "Artur Lucas",
+    "status": "in_progress",
+  },
+  {
+    "name": "Exercícios Javascript",
+    "description": "Repo: https://github.com/ArturLucas09/exercicios-js",
+    "responsible": "Artur Lucas",
+    "status": "in_progress",
   },
 ];
+
